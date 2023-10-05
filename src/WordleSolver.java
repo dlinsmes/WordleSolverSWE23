a3516c0b8809400d81c1aad1dc13d14b9efc7b8e
Vaughn Bosu <vwbosu@gmail.com>
2023-10-05 09:07:49 -0700

    methods decided, yippie!

--- src/WordleSolver.java
+++ src/WordleSolver.java
@@ -1,3 +1,37 @@
+import java.util.ArrayList;
+
 public class WordleSolver {
+		private static String answer;
+
+		private static ArrayList<String> possibleWords;
+		private static ArrayList<String> possibleAnswer;
+
+
+		public static void main(String[] args) {
+
+		}
+
+		public static void setAnswer() {
+//			set answer
+		}
+
+		public static void importData() {
+//				import csv data for possible words
+		}
+
+		public int[] giveFeedback(String guess) {
+
+				return null;
+		}
+
+		public static void attemptGuess(){
+
+		}
+		public static void narrowList(){
+
+		}
+
+
+
 
 }
