b86cf76d4bff006cc6139859b1bc097417d65eb1
Vaughn Bosu <vwbosu@gmail.com>
2023-10-13 09:08:05 -0700

    used "soare" as first word, auto formatted. Yippe!

--- src/WordleSolver.java
+++ src/WordleSolver.java
@@ -3,17 +3,18 @@ import java.util.ArrayList;
 public class WordleSolver {
 		private static String answer;
 
-		private static ArrayList<String> possibleWords = new ArrayList<>();;
+		private static ArrayList<String> possibleWords = new ArrayList<>();
+		;
 		private static ArrayList<String> possibleAnswers = new ArrayList<>();
 		private final static int GREEN = 2;
 		private final static int YELLOW = 1;
 		private final static int GREY = 0;
 
 
-
 		public static void main(String[] args) {
 
 				int guessesAttempted = 0;
+				boolean firstGuess = true;
 
 				importData("Words.csv");
 				setAnswer();
@@ -21,13 +22,18 @@ public class WordleSolver {
 				String guess = "Filler";
 				int[] feedback;
 
-				while(!guess.equals(answer)){
-					guess = attemptGuess();
-					System.out.println("Guessing: " + guess);
-					feedback = giveFeedback(guess);
-					narrowList(guess, feedback);
-					System.out.println("There are " + possibleWords.size() + " possible words left");
-					guessesAttempted++;
+				while (!guess.equals(answer)) {
+						if (firstGuess) {
+								guess = "soare";
+								firstGuess = false;
+						} else {
+								guess = attemptGuess();
+						}
+						System.out.println("Guessing: " + guess);
+						feedback = giveFeedback(guess);
+						narrowList(guess, feedback);
+						System.out.println("There are " + possibleWords.size() + " possible words left");
+						guessesAttempted++;
 				}
 
 				System.out.println(answer + " was found in " + guessesAttempted + " guesses");
@@ -35,21 +41,21 @@ public class WordleSolver {
 
 		public static void setAnswer() {
 //			set answer
-			int index = (int)(Math.random()* possibleAnswers.size());
-			answer = possibleAnswers.get(index);
+				int index = (int) (Math.random() * possibleAnswers.size());
+				answer = possibleAnswers.get(index);
 		}
 
 		public static void importData(String filename) {
 				ArrayList<String[]> data = CSVImporter.importData(filename);
 				for (int i = 1; i < data.size(); i++) {
 						String word = data.get(i)[1];
-						if(word.length() == 5) {
+						if (word.length() == 5) {
 								possibleWords.add(word);
 						}
 				}
-				for(int i = 1; i < data.size(); i++) {
+				for (int i = 1; i < data.size(); i++) {
 						String word = data.get(i)[2];
-						if(word.length() == 5) {
+						if (word.length() == 5) {
 								possibleWords.add(word);
 								possibleAnswers.add(word);
 						}
@@ -57,144 +63,136 @@ public class WordleSolver {
 				}
 		}
 
-	public static int[] giveFeedback(String guess) {
-		//check green: go through every letter of their guess and check if the letter at the index matches the final word
+		public static int[] giveFeedback(String guess) {
+				//check green: go through every letter of their guess and check if the letter at the index matches the final word
 
-		int[] feedback = new int[guess.length()];
-		char[] letters = guess.toCharArray();
-		char[] answerLetters = answer.toCharArray();
-		for(int i = 0; i < guess.length(); i++){
-			if(guess.charAt(i)==answer.charAt(i)){
-				feedback[i] = GREEN;
-				letters[i] = '&';
-				answerLetters[i] = '-';
-			}
+				int[] feedback = new int[guess.length()];
+				char[] letters = guess.toCharArray();
+				char[] answerLetters = answer.toCharArray();
+				for (int i = 0; i < guess.length(); i++) {
+						if (guess.charAt(i) == answer.charAt(i)) {
+								feedback[i] = GREEN;
+								letters[i] = '&';
+								answerLetters[i] = '-';
+						}
 
-		}
-		for(int i = 0; i < guess.length(); i++){
-			if(answer.indexOf(guess.charAt(i)) == -1){
-				//indexOf pass in a substring give you negative one if its not there
-				feedback[i]= GREY;
-				letters[i]= '&';
-			}
-		}
-		//check for yellow
-		for(int i=0; i< guess.length(); i++){
-			for(int j=0; j< answer.length(); j++){
-				if(letters[i] == answerLetters[j]){
-					feedback[i] = YELLOW;
-					answerLetters[j] = '-';
-					j = answer.length();
 				}
-			}
+				for (int i = 0; i < guess.length(); i++) {
+						if (answer.indexOf(guess.charAt(i)) == -1) {
+								//indexOf pass in a substring give you negative one if its not there
+								feedback[i] = GREY;
+								letters[i] = '&';
+						}
+				}
+				//check for yellow
+				for (int i = 0; i < guess.length(); i++) {
+						for (int j = 0; j < answer.length(); j++) {
+								if (letters[i] == answerLetters[j]) {
+										feedback[i] = YELLOW;
+										answerLetters[j] = '-';
+										j = answer.length();
+								}
+						}
 
 
+				}
+
+
+				return feedback;
 		}
 
-		for (int i : feedback){
-			System.out.print(i);
-		}
-		System.out.println();
-
-
-		return feedback;
-	}
-
-		public static String attemptGuess(){
+		public static String attemptGuess() {
 //				choose a random word from possible words and return it
-				int index = (int)(Math.random()* possibleWords.size());
+				int index = (int) (Math.random() * possibleWords.size());
 				return possibleWords.get(index);
 		}
-	public static void narrowList(String guess, int[] feedback){
+
+		public static void narrowList(String guess, int[] feedback) {
 // if there's only one green e and one gray e delete everything with multiple es
 //        make sure that you check that there is the amount of letters if there's two es remove
 //        if there's a gray you know the exact number of characters but if there's no green then
-		for(int i = 0; i < 5; i++){
-			if(feedback[i] == GREEN){
-				// System.out.println("Green");
-				for (int k = 0; k < possibleWords.size(); k++){
-					if(possibleWords.get(k).charAt(i) != guess.charAt(i)){
-						possibleWords.remove(k);
-						k--;
-					}
+				for (int i = 0; i < 5; i++) {
+						if (feedback[i] == GREEN) {
+								// System.out.println("Green");
+								for (int k = 0; k < possibleWords.size(); k++) {
+										if (possibleWords.get(k).charAt(i) != guess.charAt(i)) {
+												possibleWords.remove(k);
+												k--;
+										}
+								}
+						}
 				}
-			}
-		}
-		for(int i = 0; i < 5; i++){
-			if(feedback[i] == YELLOW){
-				// System.out.println("yellow");
-				for (int k = 0; k < possibleWords.size(); k++) {
-					boolean haveYellow = false;
-					// get rid of them or something if they don't have any yellows
-					for(int j = 0; j < 5; j++){
+				for (int i = 0; i < 5; i++) {
+						if (feedback[i] == YELLOW) {
+								// System.out.println("yellow");
+								for (int k = 0; k < possibleWords.size(); k++) {
+										boolean haveYellow = false;
+										// get rid of them or something if they don't have any yellows
+										for (int j = 0; j < 5; j++) {
 //                    if any of the letters is the same as guess that was yellow thats not at the same location then make sure we don't delete it from list
-						if(possibleWords.get(k).charAt(j)==guess.charAt(i) && j != i)
-							haveYellow = true;
-					}
-					if(!haveYellow){
-						possibleWords.remove(k);
-						k--;
-					}
+												if (possibleWords.get(k).charAt(j) == guess.charAt(i) && j != i)
+														haveYellow = true;
+										}
+										if (!haveYellow) {
+												possibleWords.remove(k);
+												k--;
+										}
 
+								}
+						}
 				}
-			}
-		}
 
 
-		for(int i = 0; i < 5; i++){
-			if(feedback[i] == GREY){
-				// System.out.println("grey 1 ");
-				for (int k = 0; k < possibleWords.size(); k++) {
-					boolean haveGrey = false;
-					// get rid of them or something if they don't have any yellows
-					for(int j = 0; j < 5; j++){
+				for (int i = 0; i < 5; i++) {
+						if (feedback[i] == GREY) {
+								// System.out.println("grey 1 ");
+								for (int k = 0; k < possibleWords.size(); k++) {
+										boolean haveGrey = false;
+										// get rid of them or something if they don't have any yellows
+										for (int j = 0; j < 5; j++) {
 //                    if any of the letters is the same as guess that was yellow thats not at the same location then make sure we don't delete it from list
-						if(possibleWords.get(k).charAt(j)==guess.charAt(i) && j == i)
-							haveGrey = true;
-					}
-					if(haveGrey){
-						possibleWords.remove(k);
-						k--;
-					}
+												if (possibleWords.get(k).charAt(j) == guess.charAt(i) && j == i)
+														haveGrey = true;
+										}
+										if (haveGrey) {
+												possibleWords.remove(k);
+												k--;
+										}
 
 
+								}
+						}
 				}
-			}
-		}
-		for(int i = 0; i < 5; i++){
-			int greyInstanceCounter = 0;
-			if(feedback[i] == GREY) {
-				// System.out.println("grey 2");
-				for (int j = 0; j < 5; j++) {
-					if (guess.charAt(i) == guess.charAt(j) && feedback[j] == 0) {
-						greyInstanceCounter++;
-					}
+				for (int i = 0; i < 5; i++) {
+						int greyInstanceCounter = 0;
+						if (feedback[i] == GREY) {
+								// System.out.println("grey 2");
+								for (int j = 0; j < 5; j++) {
+										if (guess.charAt(i) == guess.charAt(j) && feedback[j] == 0) {
+												greyInstanceCounter++;
+										}
+								}
+								int totalinstance = countLetter(guess, guess.charAt(i)) - greyInstanceCounter;
+								for (int k = 0; k < possibleWords.size(); k++) {
+										int eachLetterInstance = countLetter(possibleWords.get(k), guess.charAt(i));
+										if (eachLetterInstance > totalinstance || eachLetterInstance < totalinstance) {
+												possibleWords.remove(k);
+												k--;
+										}
+
+								}
+						}
 				}
-				int totalinstance = countLetter(guess, guess.charAt(i)) - greyInstanceCounter;
-				for (int k = 0; k < possibleWords.size(); k++) {
-					int eachLetterInstance = countLetter(possibleWords.get(k), guess.charAt(i));
-					if (eachLetterInstance > totalinstance || eachLetterInstance < totalinstance) {
-						possibleWords.remove(k);
-						k--;
-					}
-
-
-				}
-			}
-		}
-		}
-
-
-	public static int countLetter(String word, char letter) {
-			int count = 0;
-			for(int i = 0; i < word.length(); i++){
-				if(word.charAt(i) == letter) {
-						count++;
-				}
-			}
-			return count;
 		}
 
 
-
+		public static int countLetter(String word, char letter) {
+				int count = 0;
+				for (int i = 0; i < word.length(); i++) {
+						if (word.charAt(i) == letter) {
+								count++;
+						}
+				}
+				return count;
+		}
 }
